9389293b7bbf9fb6657a971afd77ccfe9f18a77d
Stephen Murray <stephen.patrick.murray+github@gmail.com>
2014-08-19 13:26:28 -0400

    comments for game.js

--- src/game.js
+++ src/game.js
@@ -1,3 +1,11 @@
+/**
+ * game.js
+ *
+ * This module houses the high-level instantiation and event binding for the
+ * entire game. If interfaces need to be exposes to the global scope or parent
+ * namespace, it should be done so in this module.
+ */
+
 define('game',
 
 ['Canvas', 'Player', 'vent', 'util'],
@@ -11,38 +19,44 @@ function( Canvas, Player, vent, util ){
         bgCanvas,
         game;
 
+    // Create the background canvas used for the starfield effect
+    //   -- this is mostly being utlized for a performance boost
     bgCanvas = document.createElement('canvas');
     bgCanvas.id = 'bg-canvas';
     bgCanvas.height = window.innerHeight;
     bgCanvas.width = window.innerWidth;
     util.getById('game-container').appendChild(bgCanvas);
 
+    // Create the canvas stage that we'll be using for primary game entities
     canvas = new Canvas('game-container', {
         width: window.innerWidth,
         height: window.innerHeight,
         bgColor: "#000"
     });
 
+    // Create the player entity
     player = new Player(canvas);
 
+    // Create an object we can expose as an interface for debugging
+    // (modules shouldn't be touch this object)
     game = {
         canvas: canvas,
         player: player
     };
 
+    // Couple DOM events to our custom event emitter
     document.addEventListener('keydown', function( ev ){
         vent.emit('keydown', ev);
     });
-
     document.addEventListener('keyup', function( ev ){
         vent.emit('keyup', ev);
     });
-
     document.addEventListener('DOMContentLoaded', function(){
         vent.emit('create', game);
         vent.emit('start', game);
     });
 
+    // Expose the interface
     window.game = game;
 
     return game;
