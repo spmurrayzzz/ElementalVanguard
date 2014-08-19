318bf4da71f2c5b65785e73022cfc31a0a349ec0
Stephen Murray <stephen.patrick.murray+github@gmail.com>
2014-08-19 18:43:53 -0400

    clean audio module up a bit

--- src/lib/audio.js
+++ src/lib/audio.js
@@ -2,20 +2,18 @@ define('audio', ['jsfxr', 'vent'], function( jsfxr, vent ){
 
     'use strict';
 
-    function play( sound ){
+    function register( namespace, sound ){
         var player = new Audio(),
             soundURL = jsfxr(sound);
+
         player.src = soundURL;
-        player.play();
+
+        vent.on(namespace, function(){
+            player.play(sound);
+        });
     }
 
-    vent.on('shoot', function(){
-        play([0,,0.0243,,0.2334,0.7546,,-0.3971,,,,,,,,,,,1,,,,,0.33]);
-    });
-
-
-    vent.on('kaboom!', function(){
-        play([3,,0.233,0.3771,0.67,0.0656,,-0.29,,,,,,,,0.75,-0.24,0.34,0.15,-0.18,0.76,,,0.31]);
-    });
+    register('shoot', [0,,0.0243,,0.2334,0.7546,,-0.3971,,,,,,,,,,,1,,,,,0.33]);
+    register('kaboom!', [3,,0.233,0.3771,0.67,0.0656,,-0.29,,,,,,,,0.75,-0.24,0.34,0.15,-0.18,0.76,,,0.31]);
 
 });
