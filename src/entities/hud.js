4a8933335f44bc704021c6b1bb164ae7a885cf34
Stephen Murray <stephen.patrick.murray+github@gmail.com>
2014-08-28 17:24:48 -0400

    show N/10 for `enemies let by` display

--- src/entities/hud.js
+++ src/entities/hud.js
@@ -81,7 +81,7 @@ function( vent, util ){
             );
         },
         enemyCount: function(){
-            enemyElem.innerHTML = 'Enemies let by: ' + enemyCount;
+            enemyElem.innerHTML = 'Enemies let by: ' + enemyCount + '/10';
         }
 
     };
